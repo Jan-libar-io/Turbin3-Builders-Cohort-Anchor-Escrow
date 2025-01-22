818eb9152543379d6cfe840d656d8b6676e37731
Jan J <jan.jankowski@libar.io>
2025-01-22 22:40:53 +0100

    escrow implementation

--- programs/anchor_escrow/src/lib.rs
+++ programs/anchor_escrow/src/lib.rs
@@ -1,16 +1,25 @@
 use anchor_lang::prelude::*;
 
+use crate::instructions::*;
+
 declare_id!("8ffTQaLBEGtSAXGKaNpR1UhrftW6MK2gyWT3qXRUimX3");
+
+pub mod state;
+pub mod instructions;
 
 #[program]
 pub mod anchor_escrow {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn make(ctx: Context<Make>, seed: u64, receive_amount: u64, deposit: u64) -> Result<()> {
+        ctx.accounts.init_escrow(seed, receive_amount, &ctx.bumps)?;
+        ctx.accounts.deposit(deposit)?;
+        Ok(())
+    }
+
+    pub fn take(ctx: Context<Take>) -> Result<()> {
+        ctx.accounts.deposit()?;
+        ctx.accounts.withdraw_and_close_vault()?;
         Ok(())
     }
 }
-
-#[derive(Accounts)]
-pub struct Initialize {}
